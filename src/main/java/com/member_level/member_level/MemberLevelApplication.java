b61b4311972ad795e3c0b21ae578494aa7a13d69
ManSokleaksa <man.sokleaksa2020@gmail.com>
2024-07-01 11:44:58 +0700

    First project

---
+++ src/main/java/com/member_level/member_level/MemberLevelApplication.java
@@ -0,0 +1,13 @@
+package com.member_level.member_level;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberLevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberLevelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/member_level/member_level/MemberLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.member_level.member_level;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberLevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
